18f15f69f1178dbb979d794fcb30421ce85ebe09
czy <czy@czy-PC>
2012-05-08 20:07:34 +0800

    ???????

---
+++ Dev3Lib/Dev3Lib.Test/PaginationTest.cs
@@ -0,0 +1,84 @@
+﻿using Dev3Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Dev3Lib.Algorithms;
+
+namespace Dev3Lib.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for PaginationTest and is intended
+    ///to contain all PaginationTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class PaginationTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        [TestMethod()]
+        public void GetPageTest()
+        {
+            int[] items = new int[] { 1,2,3,4,5};
+
+            Pagination<int> p = new Pagination<int>(items, 2);
+
+            Assert.AreEqual(3, p._pagesCount);
+            Assert.IsTrue(p.GetPage(2).SafeIsEqual(new[] { 3, 4 }));
+
+            p = new Pagination<int>(items, 3);
+
+            Assert.AreEqual(2, p._pagesCount);
+            Assert.IsTrue(p.GetPage(2).SafeIsEqual(new[] { 4, 5 }));
+        }
+    }
+}
---
+++ Dev3Lib/Dev3Lib/Pagination.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Dev3Lib.Algorithms;
+
+namespace Dev3Lib
+{
+    public sealed class Pagination<T>
+    {
+        private readonly T[] _items;
+        private readonly int _itemCount;
+
+        public readonly int _pagesCount;
+        public Pagination(T[] items, int itemCount)
+        {
+            if (itemCount == 0)
+                throw new ArgumentException("itemCount");
+
+            _items = items;
+            _itemCount = itemCount;
+
+            _pagesCount = (int)Math.Ceiling((decimal)items.SafeCount() / itemCount);
+        }
+
+        public T[] GetPage(int pageIndex)
+        {
+            if (pageIndex > _pagesCount || pageIndex < 1)
+                return null;
+
+            T[] results = null;
+            int len = 0;
+            if (pageIndex * _itemCount > _items.Length)
+            {
+                len = _items.Length;
+                results = new T[_itemCount - pageIndex * _itemCount + _items.Length];
+            }
+            else
+            {
+                len = pageIndex * _itemCount;
+                results = new T[_itemCount];
+            }
+
+
+            int index = 0;
+            for (int i = (pageIndex - 1) * _itemCount;
+                i < len;
+                i++)
+            {
+                results[index++] = _items[i];
+            }
+
+            return results;
+        }
+
+
+
+    }
+}
