3d908f95fcc9ebfef0e15c0349573b8672351722
czy <czy@czy-PC>
2012-06-20 17:14:36 +0800

    ???????DicLocation ?Knut???????

---
+++ Dev3Lib/Dev3Lib/Web/DicExpressionBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Compilation;
+using System.ComponentModel;
+using System.CodeDom;
+
+namespace Dev3Lib.Web
+{
+    [ExpressionPrefix("dic")]
+    public class DicExpressionBuilder : ExpressionBuilder
+    {
+
+        public static object GetEvalData(string expression)
+        {
+            return DicLocation.GetLanguageValue(expression);
+        }
+
+
+        public override System.CodeDom.CodeExpression GetCodeExpression(System.Web.UI.BoundPropertyEntry entry, object parsedData, ExpressionBuilderContext context)
+        {
+            Type type1 = entry.DeclaringType;
+            PropertyDescriptor descriptor1 = TypeDescriptor.GetProperties(type1)[entry.PropertyInfo.Name];
+            CodeExpression[] expressionArray1 = new CodeExpression[1];
+            expressionArray1[0] = new CodePrimitiveExpression(entry.Expression.Trim());
+            return new CodeCastExpression(descriptor1.PropertyType, new CodeMethodInvokeExpression(new
+           CodeTypeReferenceExpression(base.GetType()), "GetEvalData", expressionArray1));
+        }
+
+        public override bool SupportsEvaluate
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Dev3Lib/Dev3Lib/Web/DicLocation.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.IO;
+using System.Xml.Linq;
+
+namespace Dev3Lib.Web
+{
+    public static class DicLocation
+    {
+
+        const string fileNameFormat = "dic.{0}.resx";
+        const string defaultFileName = "dic.resx";
+        const string defaultCultureName = "en-US";
+
+        class cacheItem
+        {
+            private readonly DateTime _createdTime = DateTime.Now;
+
+            public bool IsExpired
+            {
+                get
+                {
+                    return _createdTime.Subtract(DateTime.Now) > TimeSpan.FromHours(1);
+                }
+            }
+
+            public string Item { get; set; }
+        }
+
+        private static Dictionary<string, cacheItem> LanguageCaches
+        {
+            get
+            {
+                if (HttpContext.Current.Application["DicLocation_Cache"] == null)
+                    HttpContext.Current.Application["DicLocation_Cache"] = new Dictionary<string, cacheItem>();
+
+                return (Dictionary<string, cacheItem>)HttpContext.Current.Application["DicLocation_Cache"];
+            }
+        }
+
+        private static string GetResourceValue(string key)
+        {
+            var cultureName = Thread.CurrentThread.CurrentUICulture.Name;
+            var languageFolder = HttpContext.Current.Request.MapPath("App_GlobalResources");
+
+
+            string defaultFullFileName = Path.Combine(languageFolder, defaultFileName);
+            string fullFileName = string.Empty;
+            if (string.Compare(cultureName, defaultCultureName, StringComparison.OrdinalIgnoreCase) == 0)
+            {
+                fullFileName = defaultFullFileName;
+            }
+            else
+                fullFileName = Path.Combine(languageFolder, string.Format(fileNameFormat, cultureName));
+
+            if (!File.Exists(fullFileName))
+            {
+                if (!File.Exists(defaultFullFileName))
+                    return null;
+                else
+                    fullFileName = defaultFullFileName;
+            }
+
+            XDocument doc = XDocument.Load(fullFileName);
+            var value = (from n in doc.Descendants("data")
+                         where string.Compare(n.Attribute("name").Value, key, StringComparison.OrdinalIgnoreCase) == 0
+                         select n.Descendants("value").Single().Value)
+                   .SingleOrDefault();
+
+            return value;
+        }
+
+        public static string GetLanguageValue(string entryKey)
+        {
+            cacheItem item;
+            if (LanguageCaches.TryGetValue(entryKey, out item))
+            {
+                if (item.IsExpired)
+                    LanguageCaches[entryKey] = new cacheItem
+                    {
+                        Item = GetResourceValue(entryKey)
+                    };
+                else
+                    return item.Item;
+            }
+            else
+            {
+                LanguageCaches.Add(entryKey, new cacheItem
+                {
+                    Item = GetResourceValue(entryKey)
+                });
+            }
+
+            return LanguageCaches[entryKey].Item;
+        }
+    }
+}
