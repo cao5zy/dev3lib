0f4d5610f419f362158e70e94b612498d491ef7f
hg <hg@localhost>
2013-01-01 22:27:50 +0800

    ADD WHERE

---
+++ Dev3Lib/Dev3Lib/Sql/IWhere.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dev3Lib.Sql
+{
+    public interface IWhere
+    {
+        IWhere And(IWhere where);
+        IWhere Or(IWhere where);
+        string ToWhereClause();
+        string ColumnName { get; set; }
+        string ParamName { get; set; }
+        object Value { get; set; }
+        Comparison Comparison { get; set; }
+    }
+}
---
+++ Dev3Lib/Dev3Lib/Sql/Where.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dev3Lib.Sql
+{
+    public sealed class Where : IWhere
+    {
+        private object _value;
+        private string _paramName;
+        private string _columnName;
+        private Comparison _comparison;
+        private IWhere _nextWhere;
+        private bool _isAnd;
+        
+        public IWhere And(IWhere where)
+        {
+            _isAnd = true;
+            _nextWhere = where;
+            return this;
+        }
+
+        public IWhere Or(IWhere where)
+        {
+            _nextWhere = where;
+            return this;
+        }
+
+        public string ColumnName
+        {
+            get
+            {
+                return _columnName;
+            }
+            set
+            {
+                _columnName = value;
+            }
+        }
+
+        public string ParamName
+        {
+            get
+            {
+                return _paramName;
+            }
+            set
+            {
+                if (value.StartsWith("@"))
+                    _paramName = value;
+                else
+                    _paramName = string.Format("@{0}", value);
+            }
+        }
+
+        public object Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public Comparison Comparison
+        {
+            get
+            {
+                return _comparison;
+            }
+            set
+            {
+                _comparison = value;
+            }
+        }
+
+
+        public string ToWhereClause()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (_nextWhere == null)
+            {
+                sb.Append(ColumnName)
+                    .Append(Comparison.ToOperator())
+                    .Append(ParamName);
+            }
+            else
+            {
+                sb.Append("(")
+                    .Append(ColumnName)
+                    .Append(Comparison.ToOperator())
+                    .Append(ParamName);
+                if (_isAnd)
+                    sb.Append(" AND ");
+                else
+                    sb.Append(" OR ");
+
+                sb.Append(_nextWhere.ToWhereClause())
+                    .Append(")");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
