e791e0287cdef870bddb6a98dfc56fed1d6a733e
hg <hg@localhost>
2013-01-01 23:00:05 +0800

    abstractselector

---
+++ Dev3Lib/Dev3Lib/Sql/AbstractSelector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Dev3Lib.Sql
+{
+    public abstract class AbstractSelector
+    {
+        public abstract T Read<T>(Func<IDataReader, T> convert,
+            string sql,
+            IWhere where);
+
+        public abstract List<T> Return<T>(Func<IDataReader, T> convert,
+            string sql,
+            IWhere where);
+
+        public abstract int Count(string sql,
+            IWhere where);
+    }
+}
