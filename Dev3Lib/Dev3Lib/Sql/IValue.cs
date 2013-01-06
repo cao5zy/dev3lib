7db45a5e0647591c3db0a2051f96aa72fc3dadfd
hg <hg@localhost>
2013-01-06 23:09:03 +0800

    add IValue

---
+++ Dev3Lib/Dev3Lib/Sql/IValue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dev3Lib.Sql
+{
+    public interface IValue
+    {
+        IValue Append(IValue value);
+        string ColumnName { get; set; }
+        string ParamName { get; set; }
+        object Value { get; set; }
+        List<IValue> ToList();
+    }
+}
