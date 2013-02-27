70501b53b5d1d9468e685d794c76ef187c05e29c
cao <cao@localhost>
2013-02-27 23:23:13 +0800

    add dependency factory

---
+++ Dev3Lib/Dev3Lib.Test/DependencyFactoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dev3Lib.Web;
+using Autofac;
+
+namespace Dev3Lib.Test
+{
+    [TestClass]
+    public class DependencyFactoryTest
+    {
+        [TestMethod]
+        public void Resolve_Normal_ReturnInstance()
+        {
+            DependencyFactory.SetContainer(() =>
+            {
+                ContainerBuilder builder = new ContainerBuilder();
+                builder.RegisterType<TestClass>();
+
+                return builder.Build();
+            });
+
+            Assert.IsNotNull(DependencyFactory.Resolve<TestClass>());
+        }
+
+        class TestClass
+        {
+            public string Name { get { return "hello"; } }
+        }
+
+    }
+}
---
+++ Dev3Lib/Dev3Lib/Web/DependencyFactory.cs
@@ -0,0 +1,68 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Dev3Lib.Web
+{
+    public static class DependencyFactory
+    {
+        const string _dependencyFactory = "{EE9E78D1-7740-4C18-B139-3A323E79E238}";
+        private static Func<IContainer> _containerFunc;
+        private static readonly object _obj = new object();
+        private static IContainer _container;
+        private static IContainer Container
+        {
+            get
+            {
+                if (_container != null)
+                    return _container;
+                else
+                {
+                    if (HttpContext.Current == null)
+                    {
+                        lock (_obj)
+                        {
+                            if (_container == null)
+                            {
+                                _container = _containerFunc();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (HttpContext.Current.Application[_dependencyFactory] == null)
+                        {
+                            lock (_obj)
+                            {
+                                if (HttpContext.Current.Application[_dependencyFactory] == null)
+                                {
+                                    _container = _containerFunc();
+                                    HttpContext.Current.Application[_dependencyFactory] = _container;
+                                }
+                            }
+                        }
+                        else
+                            _container = (IContainer)HttpContext.Current.Application[_dependencyFactory];
+
+                    }
+                }
+
+                return _container;
+            }
+        }
+
+        public static void SetContainer(Func<IContainer> setContainer)
+        {
+            _containerFunc = setContainer;
+        }
+
+        public static T Resolve<T>()
+        {
+            return Container.Resolve<T>();
+        }
+
+    }
+}
