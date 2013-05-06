301267e6de9df1c809c57e95b6fb5bb4b2e41d96
cao <cao@localhost>
2013-05-06 19:29:37 +0800

    add enum to list

---
+++ Dev3Lib/Dev3Lib/Util/DateTimeForEach.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dev3Lib.Util
+{
+    public sealed class DateTimeForEach
+    {
+        public enum StepBy { 
+            Year,
+            Month,
+            Day,
+            Hour,
+            Min,
+            Sec,
+            TimeSpan,
+        }
+        private DateTime _start, _end;
+
+        public DateTimeForEach(DateTime start, DateTime end)
+        { }
+    }
+}
