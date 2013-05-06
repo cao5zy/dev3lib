3cadf07de0cbe797757777aed2b11686f2e3b58c
cao <cao@localhost>
2013-05-06 19:29:37 +0800

    add enum to list

---
+++ Dev3Lib/Dev3Lib.Test/EnumExtensionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Dev3Lib.Util;
+
+namespace Dev3Lib.Test
+{
+    [TestClass]
+    public class EnumExtensionTest
+    {
+        enum testEnum
+        { 
+            Hello,
+            AB,
+            C_D,
+            AbCd,
+        }
+        [TestMethod]
+        public void ToList_Success()
+        {
+            testEnum e = new testEnum();
+            var list = EnumExtension.ToList<testEnum>();
+
+            Assert.IsTrue(list.Count == 4);
+            Assert.AreEqual("Hello", list[0].Value);
+            Assert.AreEqual("A B", list[1].Value);
+            Assert.AreEqual("C/D", list[2].Value);
+            Assert.AreEqual("Ab Cd", list[3].Value);
+        }
+    }
+}
---
+++ Dev3Lib/Dev3Lib/Util/EnumExtension.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Dev3Lib.Util
+{
+    public static class EnumExtension
+    {
+        private static Regex _OnlyText = new Regex(@"[A-Z]{1}[a-z]*");
+        public static List<KeyValuePair<int, string>> ToList<T>()
+        {
+            List<KeyValuePair<int, string>> list = new List<KeyValuePair<int, string>>();
+
+            Type t = typeof(T);
+            var names = Enum.GetNames(t);
+            var values = Enum.GetValues(t);
+
+            if (values.Length != 0)
+            {
+                int index = 0;
+                foreach (var n in values)
+                {
+                    list.Add(new KeyValuePair<int, string>(Convert.ToInt32(n), ConvertName(names[index++])));
+                }
+            }
+            return list;
+        }
+
+        private static string ConvertName(string originalEnumName)
+        {
+            var names = originalEnumName.Split(new char[]{'_'});
+            if (names.Length > 1)
+                return string.Join("/", names);
+
+            var matchs = _OnlyText.Matches(originalEnumName);
+            if (matchs.Count != 0)
+            {
+                List<string> slist = new List<string>();
+                foreach (Match m in matchs)
+                {
+                    slist.Add(m.Value);
+                }
+
+                return string.Join(" ", slist.ToArray());
+            }
+
+            return originalEnumName;
+        }
+    }
+}
