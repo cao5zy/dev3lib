038b8f57f3c34df9c92d1a7884cc24d468e5480a
cao <cao@localhost>
2013-06-08 11:48:33 +0800

    ???delete

---
+++ Dev3Lib/Dev3Lib/Sql/ISqlDelete.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dev3Lib.Sql
+{
+    public interface ISqlDelete
+    {
+        void Delete(string tableName, WhereClause where);
+    }
+}
