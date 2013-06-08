83c769860236cf1121b8ffbac83704c50879365e
cao <cao@localhost>
2013-06-08 11:48:33 +0800

    ???delete

---
+++ Dev3Lib/Dev3Lib.Test/SqlDelete_Test.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+using Dev3Lib.Sql;
+
+namespace Dev3Lib.Test
+{
+    [TestClass()]
+    public class SqlDelete_Test
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            DependencyFactory.PartialSetContainer(builder =>
+            {
+                builder.RegisterType<SqlDelete>().As<ISqlDelete>();
+                builder.RegisterType<DefaultDbContext>().As<IDbContext>().WithParameter(new TypedParameter(typeof(string), "Initial Catalog=tbaDATA;Data Source=(local)\\Sqlexpress;Integrated Security=true"));
+            });
+        }
+
+        [TestMethod]
+        public void Delete_Success()
+        {
+            using (DependencyFactory.BeginScope())
+            {
+                DependencyFactory.Resolve<IDbContext>().BeginTransaction();
+                var delete = DependencyFactory.Resolve<ISqlDelete>();
+                delete.Delete("tbltbaerror", new WhereClause(96, "error_id"));
+            }
+        }
+    }
+}
---
+++ Dev3Lib/Dev3Lib/Sql/SqlDelete.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace Dev3Lib.Sql
+{
+    public class SqlDelete : ISqlDelete
+    {
+        private IDbContext _dbContext;
+        private string _debugSql = string.Empty;
+
+        public SqlDelete(IDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public string DebugSql
+        {
+            get
+            {
+                return _debugSql;
+            }
+        }
+
+        public void Delete(string tableName, WhereClause where)
+        {
+            string sql = string.Format("delete from {0} where {1}", tableName, where.Clause);
+
+            _debugSql = sql;
+
+            using (var cmd = _dbContext.Connection.CreateCommand())
+            {
+                cmd.Transaction = _dbContext.Transaction;
+
+                cmd.CommandText = sql;
+
+                GenerateParameters(where, cmd.Parameters);
+
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void GenerateParameters(WhereClause where, SqlParameterCollection paramColl)
+        {
+            var valueDic = where.ToNameValues();
+
+            List<SqlParameter> list = new List<SqlParameter>(valueDic.Count);
+
+            foreach (var keypair in valueDic)
+            {
+                paramColl.AddWithValue(keypair.Key, keypair.Value);
+            }
+
+        }
+    }
+}
