1d316e311f01f6ca2af6dbf1db34079b52a532eb
cao <cao@localhost>
2013-11-29 17:48:21 +0800

    add datavalidation

---
+++ Dev3Lib/Dev3Lib/DataValidations/CellPhoneValidationAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Dev3Lib.DataValidations
+{
+    public class CellPhoneValidationAttribute : RegularExpressionAttribute
+    {
+        public CellPhoneValidationAttribute(string errorMessage = "手机号码格式不正确")
+            : base("^1[0-9]{10}$")
+        {
+            this.ErrorMessage = errorMessage;
+        }
+    }
+}
